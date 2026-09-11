b799059245db02eb626b9361fa712aef5f4550df
bakoferenc20 <bakoferenc20@gmail.com>
2026-09-11 22:33:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='szomuhely-v1';
-const ASSETS=['./','./index.html','./manifest.json','./chapters/wirtschaft-01.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='szomuhely-v2';
+const ASSETS=['./','./index.html','./manifest.json','./chapters/index.json','./chapters/wirtschaft-01.json','./chapters/wirtschaft-02.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
